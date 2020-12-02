dc118860108c56ced7de2e924adb80c5d07b2cb4
wz2cool@live.cn <wz2cool@live.cn>
2020-12-02 11:09:54 +0800

    init

--- src/main/java/com/github/wz2cool/demo/rocketmqdemo/RocketmqDemoApplication.java
+++ src/main/java/com/github/wz2cool/demo/rocketmqdemo/RocketmqDemoApplication.java
@@ -2,11 +2,14 @@ package com.github.wz2cool.demo.rocketmqdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class RocketmqDemoApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(RocketmqDemoApplication.class, args);
     }
 
---
+++ src/main/java/com/github/wz2cool/demo/rocketmqdemo/service/ProducerService.java
@@ -0,0 +1,45 @@
+package com.github.wz2cool.demo.rocketmqdemo.service;
+
+import com.github.wz2cool.demo.rocketmqdemo.model.User;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class ProducerService {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Resource
+    private RocketMQTemplate rocketMQTemplate;
+
+    @Value(value = "${boot.rocketmq.topic}")
+    private String springTopic;
+
+    @Value(value = "${boot.rocketmq.topic.user}")
+    private String userTopic;
+
+    @Value(value = "${boot.rocketmq.tag}")
+    private String tag;
+
+    public SendResult sendString(String message) {
+        // 发送 String 类型的消息
+        // 调用 RocketMQTemplate 的 syncSend 方法
+        SendResult sendResult = rocketMQTemplate.syncSend(springTopic + ":" + tag, message);
+        logger.info("syncSend String to topic {} sendResult={} \n", springTopic, sendResult);
+        return sendResult;
+    }
+
+    public SendResult sendUser(User user) {
+        // 发送 User
+        SendResult sendResult = rocketMQTemplate.syncSend(userTopic, user);
+        logger.info("syncSend User to topic {} sendResult= {} \n", userTopic, sendResult);
+        return sendResult;
+    }
+
+}
