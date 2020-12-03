57e0f71db71416860050ab8072a727df8a9ea6db
wz2cool@live.cn <wz2cool@live.cn>
2020-12-03 14:28:29 +0800

    add

---
+++ rocketmq-consumer/src/main/java/com/github/wz2cool/demo/rocketmqproducer/RocketmqProducerApplication.java
@@ -0,0 +1,13 @@
+package com.github.wz2cool.demo.rocketmqproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqProducerApplication.class, args);
+    }
+
+}
